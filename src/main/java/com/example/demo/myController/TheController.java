e331d08091f6ac91adede0b6d048d3e72b59416a
Dengzhanhong <dengzhanhongdarren@outlook.com>
2019-10-06 23:22:21 +0800

    second commit ：）

--- src/main/java/com/example/demo/myController/TheController.java
+++ src/main/java/com/example/demo/myController/TheController.java
@@ -4,7 +4,7 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -21,44 +21,45 @@ public class TheController {
     }
 
     // 注册
-    @RequestMapping("/register")
+    @GetMapping("/register")
     public String register(){
         return "regist";
     }
 
     @RequestMapping("doRegister")
-    public String doRegister(User user, Map<String,Object> map){
+    public String doRegister(User user, Model model){
         User user1 = userService.findByUsername(user.getUsername());
-        if (user1 == null) {
-            map.put("msg", "用户名已存在");
+        if (user1 != null) {
+            model.addAttribute("registerState","用户名已存在");
             return "regist";
         }
         User user2 = userService.findByEmail(user.getEmail());
-        if (user1 == null) {
-            map.put("msg", "邮箱已被注册");
+        if (user2 != null) {
+            model.addAttribute("registerState", "邮箱已被注册");
             return "regist";
         }
         userService.insertUser(user);
-        map.put("msg","注册成功！");
         return "login";
     }
 
 
     // 登陆
     @RequestMapping("/login")
-    public String login(){
+    public String login(Model model){
+        model.addAttribute("state","");
         return "login";
     }
 
-    @RequestMapping("doLogin")
-    public String doLogin(User user,Map<String,Object> map){
+    @RequestMapping("/doLogin")
+    public String doLogin(User user,Model model){
         User user1 = userService.getUser(user.getEmail(), user.getPassword());
-        if (user1 == null) {
-            map.put("msg","邮箱或密码错误");
+        System.out.println(user1);
+        if (user1 != null) {
+            model.addAttribute("state","用户名或密码错误");
             return "login";
         }
         else {
-            map.put("msg","登陆成功");
+            model.addAttribute("username",user.getUsername());
             return "index";
         }
     }
